459b610f286339f520c611283d12c96331c7641d
Thomas Rager <tomrager@yahoo.com>
2020-10-04 16:39:11 -0400

    Scheduler Layout is coming together...working on DRY code to display each hour correctly

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//Array of objects for each"" working hour
+var workingHours=["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+
+//Sets displays current date at the top of the daily scheduler using Moment.js
 var today = moment().format("dddd, MMMM Do");
-console.log(today);
 $("#currentDay").text(today);
+
+//Setting up hour blocks using Moment.js
+var hoursWorked = 0;
+var startingHour=moment().hour(9);
+$(".hour").each(function(hoursWorked){
+    moment().hour(startingHour+hoursWorked);
+    var hourDisplayed=moment().format("hA");
+    $(".hour").text(hourDisplayed);
+})
