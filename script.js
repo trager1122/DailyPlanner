97ff169bbd181b70d37962efc0fc2c05f581312f
Thomas Rager <tomrager@yahoo.com>
2020-10-05 12:58:34 -0400

    still working on color-coding time blocks

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
-//Array of objects for each"" working hour
-var workingHours=["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+$(document).ready(function () {
+//   //Array of objects for each"" working hour
+//   var workingHours = [
+//     "9AM",
+//     "10AM",
+//     "11AM",
+//     "12PM",
+//     "1PM",
+//     "2PM",
+//     "3PM",
+//     "4PM",
+//     "5PM",
+//   ];
+  //Sets displays current date at the top of the daily scheduler using Moment.js
+  var today = moment().format("dddd, MMMM Do");
+  $("#currentDay").text(today);
 
-//Sets displays current date at the top of the daily scheduler using Moment.js
-var today = moment().format("dddd, MMMM Do");
-$("#currentDay").text(today);
-
-//Setting up hour blocks using Moment.js
-$(".hour").each(function(hoursWorked){
+  //Setting up hour blocks using Moment.js
+  $(".hour").each(function (hoursWorked) {
     let today = moment();
     today.hour(9 + hoursWorked);
     let date = moment(today).format("hA");
     $(this).text(date);
-})
+  });
 
-//Color-coding time blocks based upon their time status of past, current, future
-$("textarea").each(function(numText){
-        let today = moment();
-        today.hour(9+numText);
-        let blockedHour=parseInt(moment(today).format("H"));
-        console.log (blockedHour);
-        let currentHour= parseInt(moment().format("H"));
-        console.log(currentHour);
-        if (blockedHour < currentHour) {
-            $("textarea").toggleClass("past", "future");
-        }
-        else if (blockedHour === currentHour){
-            $("textarea").toggleClass("current", "future");
-        }
-    
-})
+  //Color-coding time blocks based upon their time status of past, current, future
+  $("textarea").each(function (numText) {
+    let today = moment();
+    today.hour(9 + numText);
+    let blockedHour = parseInt(moment(today).format("H"));
+    console.log(blockedHour);
+    let currentHour = parseInt(moment().format("H"));
+    console.log(currentHour);
+    if (blockedHour > currentHour) {
+      $("textarea").toggleClass("future");
+    } else if (blockedHour === currentHour) {
+      $("textarea").toggleClass("present", "future");
+    } else if (blockedHour < currentHour) {
+      $("textarea").toggleClass("past", "present");
+    }
+  });
+});
